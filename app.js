9e39530425cd576a6a13fb1f1670768fd2664c7a
charly rapin <charlymatt@icloud.com>
2023-12-14 00:07:26 -0700

    added phone number to form and improve design of home page

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 function alertButton() {
     alert("helloWorld");
    }
+function toggleContactMethod() {
+    const preference = document.getElementById('communicationPreference').value;
+    const emailDiv = document.getElementById('email');
+    const phoneDiv = document.getElementById('phone'); 
+    if (preference === 'email') {
+      emailDiv.style.display = 'block';
+      phoneDiv.style.display = 'none';
+    } else if (preference === 'phone') {
+      emailDiv.style.display = 'none';
+      phoneDiv.style.display = 'block';
+    }
+  }
+
